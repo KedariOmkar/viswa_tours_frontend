3e6896634cf32724abc7cf107f04d848ba40a8a4
Omkar Kedari <on2xc40@gmail.com>
2025-01-09 11:21:36 +0530

    updated the app file

--- app.py
+++ app.py
@@ -8,16 +8,6 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 # Creating mysql client
 
 
-# List to store tour data
-tours = []
-db = mysql.connector.connect(
-    host="localhost",
-    username="root",
-    password="root",
-    database="ascendtours"
-)
-
-cursor = db.cursor()
 
 @app.route("/")
 def home():
@@ -38,14 +28,6 @@ def submit_contact():
     # Here you can process the data, e.g., save it to the database or send an email
     print(f"Received contact from {name}, Mobile: {mobile}, Date: {date_planning}, Reference: {reference}, Tour Type: {tour_type}")
 
-
-    # inserting into database
-    cursor.execute(
-    f"INSERT INTO enquiry (name, mobile, plan_date, referenced_by, tour_type) VALUES ('{name}', '{mobile}', '{date_planning}', '{reference}', '{tour_type}');"
-    )
-    db.commit()
-
-    print("Inserted Successfully")
 
     # Redirect or render a success page
     return redirect('/')  # You can create a thank you page or return a message
@@ -68,48 +50,6 @@ def admin():
 
     return render_template("admin_form.html")
 
-@app.route('/add_tour', methods=['POST'])
-def add_tour():
-    tour_name = request.form['tour_name']
-    tour_location = request.form['tour_location']
-    tour_price = request.form['tour_price']
-    tour_type = request.form['tour_type']
-    tour_highlights = request.form['tour_highlights']
-
-    # Handle file uploads
-    tour_images = request.files.getlist('tour_images')
-    tour_banner = request.files['tour_banner']
-
-    # Save images
-    image_filenames = []
-    for image in tour_images:
-        if image:
-            image_filename = os.path.join(UPLOAD_FOLDER, image.filename)
-            image.save(image_filename)
-            image_filenames.append(image_filename)
-
-    # Save banner
-    if tour_banner:
-        banner_filename = os.path.join(UPLOAD_FOLDER, tour_banner.filename)
-        tour_banner.save(banner_filename)
-    else:
-        banner_filename = ""
-
-    # Create a tour dictionary
-    tour_data = {
-        "tour_name": tour_name,
-        "tour_location": tour_location,
-        "tour_price": tour_price,
-        "tour_type": tour_type,
-        "tour_highlights": tour_highlights,
-        "tour_images": image_filenames,
-        "tour_banner": banner_filename
-    }
-
-    # Add the tour to the list
-    tours.append(tour_data)
-
-    return render_template('admin.html', tours=tours)
 
 
 if __name__ == "__main__":
